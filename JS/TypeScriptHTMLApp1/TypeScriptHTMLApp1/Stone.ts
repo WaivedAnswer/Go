f544818a65fde8e153b237719590e42e45423033
Quinn Ramsay <quinn.ramsay@ihsmarkit.com>
2017-06-02 10:49:34 -0600

    playing around with Typescript
    initial classes for typescript go

---
+++ JS/TypeScriptHTMLApp1/TypeScriptHTMLApp1/Stone.ts
@@ -0,0 +1,19 @@
+﻿class Stone {
+
+    teamId: TeamIds;
+
+    constructor(teamId: TeamIds) {
+        this.teamId = teamId;
+    }
+
+    display() {
+        return this.teamId;
+    }
+
+}
+
+enum TeamIds {
+    None,
+    Black,
+    White
+}
---
+++ JS/TypeScriptHTMLApp1/TypeScriptHTMLApp1/board.ts
@@ -0,0 +1,46 @@
+﻿class Board {
+
+    private values: Stone[][];
+    height: number;
+    width: number;
+    constructor(height, width) {
+        this.initialize(height, width);
+        this.height = height;
+        this.width = width;
+    }
+
+    initialize(height, width) {
+        this.values = matrix(height, width, new Stone(TeamIds.None));
+    }
+
+    display() {
+        var displayString = "";
+        for (let i = 0; i < this.values.length; i++) {
+            for (let j = 0; j < this.values[i].length; j++) {
+                displayString += this.values[i][j].display();
+            }
+            displayString += "\n";
+        }
+        console.log(displayString);
+    }
+
+    isOccupied(x, y) {
+        if (!this.isWithinBounds(x, y))
+            return false;
+        return this.values[x][y].teamId !== TeamIds.None;
+    }
+
+    isWithinBounds(x, y) {
+        return (x <= this.width - 1 && x >= 0 && y <= this.height - 1 && y >= 0)
+    }
+
+    placeStone(x, y, stone) {
+        if (this.isOccupied(x, y) || !this.isWithinBounds(x, y)) {
+            return false;
+        }
+        else {
+            this.values[x][y] = stone;
+            return true;
+        }
+    }
+}
